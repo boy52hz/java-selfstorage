5d95659674814ef4c6996821284ef2a19bcf1449
boy52hz <ggwp.zero@gmail.com>
2020-11-23 21:33:20 +0700

    Merge 'Billy' to master branch

--- src/StorageService/ServiceCalculation.java
+++ src/StorageService/ServiceCalculation.java
@@ -1,12 +1,23 @@
 package StorageService;
-
+/***
+* This is class about service calculation
+* @author Chinnawat Kaewnongsang
+* @version 1.0
+*/
 public class ServiceCalculation {
     private double serviceRate;
-
+    /***
+    * create service calculation 
+    * @param serviceRate
+    */
     public ServiceCalculation (double serviceRate){
         this.serviceRate = serviceRate;
     }
-
+    /***
+    * Get service fee
+    * @param storageArea
+    * @return service fee calculated from service rate mutiply storage area
+    */
     public double getServiceFee(double storageArea){
         return (serviceRate * storageArea);
     }
