89ada3a5788fed380c6af9c5e303c14015cdae58
boy52hz <ggwp.zero@gmail.com>
2020-11-23 21:51:16 +0700

    write new test case

--- src/SelfStorage.java
+++ src/SelfStorage.java
@@ -3,13 +3,14 @@ import StorageService.*;
 
 public class SelfStorage {
     public static void main(String[] args) {
-        StorageService storageService = new StorageService("SIT-Storage", 5, 100);
-        Person p1 = new Person("Tharadon ", " Saenmart");
+        StorageService storageService = new StorageService("SIT-Storage", 5, 50);
+        Person p1 = new Person("Tharadon", " Saenmart");
+        Person p2 = new Person("Tawan", "Tawan Muadmuenwai");
+        Person p3 = new Person("Chinnawat", "Kaewnongsang");
         Storage myStorage = storageService.rentStorage(p1, 1.0, 1.0);
-        Storage myStorage2 = storageService.rentStorage(p1, 2.0, 2.0);
-        Storage myStorage3 = storageService.rentStorage(p1, 3.0, 3.0);
+        Storage myStorage2 = storageService.rentStorage(p2, 2.0, 2.0);
+        Storage myStorage3 = storageService.rentStorage(p3, 3.0, 3.0);
         Storage myStorage4 = storageService.rentStorage(p1, 4.0, 4.0);
-        Storage myStorage5 = storageService.rentStorage(p1, 5.0, 5.0);
         storageService.unrentStorage(myStorage4);
         System.out.println(storageService);
    }
