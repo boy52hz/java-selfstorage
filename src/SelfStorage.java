90828884232e827cf7c2b27aa2d08d9a5d5520f4
boy52hz <ggwp.zero@gmail.com>
2020-11-23 23:08:30 +0700

    remove: unused package

--- src/SelfStorage.java
+++ src/SelfStorage.java
@@ -1,8 +1,6 @@
 import Base.Person;
 import StorageService.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.Month;
 
 public class SelfStorage {
